2e967fce40c36930cb58b75984b109d0fc733ab1
unknown <ignacioevil@gmail.com>
2023-08-12 19:44:48 -0300

    boton editar terminado

--- public/scriptModal.js
+++ public/scriptModal.js
@@ -1,4 +1,5 @@
 const editButtons = document.querySelectorAll(".edit-btn");
+let productId;
 
 editButtons.forEach((button) => {
 	button.addEventListener("click", openEditModal);
@@ -7,14 +8,12 @@ editButtons.forEach((button) => {
 function openEditModal(event) {
 	event.preventDefault();
 
-	const productId = this.dataset.productId;
+	productId = this.dataset.productId;
 
 	fetch(`/productos/${productId}`)
 		.then((response) => {
 			if (!response.ok) {
 				throw new Error("Hubo un Error al Cargar el Producto");
-			} else {
-				toastr.success("Producto Actualizado"); //** REVISAR AQUI MUESTRA LA NOTIFICACION PRIMERO */
 			}
 			return response.json();
 		})
@@ -30,19 +29,67 @@ function openEditModal(event) {
 		})
 		.catch((error) => {
 			console.error("Error:", error);
-			toastr.error(error.message || "Hubo un Error al Actualizar el Producto");
+			toastr.error(error.message || "Hubo un Error al Cargar el Producto");
 		});
-	toastr.options = {
-		closeButton: false,
-		debug: false,
-		newestOnTop: true,
-		progressBar: false,
-		positionClass: "toast-top-center",
-		preventDuplicates: true,
-		onclick: null,
-		showDuration: "1000",
-		hideDuration: "1500",
-		timeOut: "3000",
-		extendedTimeOut: "1000",
-	};
+
+	const aceptarButton = document.getElementById("aceptar");
+
+	if (aceptarButton) {
+		aceptarButton.addEventListener("click", function (event) {
+			event.preventDefault();
+
+			const nombre = document.getElementById("nombre").value;
+			const precio = document.getElementById("precio").value;
+			const stock = document.getElementById("stock").value;
+			const descripcion = document.getElementById("descripcion").value;
+			const imagen = document.getElementById("imagen").value;
+
+			const updatedProduct = {
+				nombre: nombre,
+				precio: precio,
+				stock: stock,
+				descripcion: descripcion,
+				imagen: imagen,
+			};
+
+			fetch(`/productos/editar/${productId}`, {
+				method: "PUT",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(updatedProduct),
+			})
+				.then((response) => {
+					if (!response.ok) {
+						throw new Error("Hubo un Error al Actualizar el Producto");
+					}
+					return response.json();
+				})
+				.then((data) => {
+					toastr.success("Producto Actualizado");
+					setTimeout(() => {
+						window.location.reload();
+					}, 2000);
+				})
+				.catch((error) => {
+					console.error("Error:", error);
+					toastr.error(
+						error.message || "Hubo un Error al Actualizar el Producto"
+					);
+				});
+		});
+	}
 }
+toastr.options = {
+	closeButton: false,
+	debug: false,
+	newestOnTop: true,
+	progressBar: false,
+	positionClass: "toast-top-center",
+	preventDuplicates: true,
+	onclick: null,
+	showDuration: "1000",
+	hideDuration: "1500",
+	timeOut: "3000",
+	extendedTimeOut: "1000",
+};
